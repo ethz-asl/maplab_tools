79fb12aeeb0f6371e34c53184d8a1b2318b69b17
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2019-11-29 15:52:09 +0100

    added publisher

---
+++ semantic_cloud_publisher/setup.py
@@ -0,0 +1,13 @@
+
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    package_dir={'':'scripts'},
+    scripts=['scripts/publisher.py']
+)
+
+setup(**setup_args)
