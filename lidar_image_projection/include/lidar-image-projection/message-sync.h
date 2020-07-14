56572d6831eeb7f611e0dd72540356f9d5f46483
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-07-14 15:24:59 +0200

    updated some proj stuff

--- lidar_image_projection/include/lidar-image-projection/message-sync.h
+++ lidar_image_projection/include/lidar-image-projection/message-sync.h
@@ -1,164 +1,181 @@
 #ifndef MAPLAB_MESSAGE_SYNC_H_
 #define MAPLAB_MESSAGE_SYNC_H_
 
-#include <map>
-#include <list>
-#include <mutex>
-#include <functional>
 #include <algorithm>
+#include <functional>
+#include <list>
+#include <map>
+#include <mutex>
+#include <ros/ros.h>
 
 #include <glog/logging.h>
 
 namespace maplab {
 
-template<typename T>
+template <typename T>
 struct ROSTimestampExtractor {
   int64_t operator()(const T& data) {
     return data->header.stamp.toNSec();
+    // return ros::Time::now().toNSec();
   }
 };
 
-template <typename FlowA, typename FlowB,
-          typename TimestampExtractorA = ROSTimestampExtractor<FlowA>,
-          typename TimestampExtractorB = ROSTimestampExtractor<FlowB>>
+static std::size_t counter = 0u;
+template <typename T>
+struct ROSTimestampExtractorHack {
+  int64_t operator()(const T& data) {
+    return counter++ * 100000256 + 1593698139793210745;
+  }
+};
+
+template <
+    typename FlowA, typename FlowB,
+    typename TimestampExtractorA = ROSTimestampExtractor<FlowA>,
+    typename TimestampExtractorB = ROSTimestampExtractor<FlowB>>
 class MessageSync {
-  public:
-     typedef std::function<void(const FlowA&, const FlowB&)>
+ public:
+  typedef std::function<void(const FlowA&, const FlowB&)>
       SynchronizedDataCallback;
 
-    explicit MessageSync(const double ts_epsilon_ns = 2.4e7) 
-      : ts_epsilon_ns_(ts_epsilon_ns), 
+  explicit MessageSync(const double ts_epsilon_ns = 2.4e7)
+      : ts_epsilon_ns_(ts_epsilon_ns),
         last_timestamp_a_(std::numeric_limits<int64_t>::min()),
-        last_timestamp_b_(std::numeric_limits<int64_t>::min()) {
-    }
+        last_timestamp_b_(std::numeric_limits<int64_t>::min()) {}
 
-    void callback1(const FlowA &flow) {
-      {
-        std::lock_guard<std::mutex> lock(m_list_a_);
-        const int64_t timestamp_a = TimestampExtractorA()(flow);
-        CHECK_GT(timestamp_a, last_timestamp_a_) << "Data from flow A not in temporal order.";
-        list_a_.emplace_back(flow);
-        last_timestamp_a_ = timestamp_a;
+  void callback1(const FlowA& flow) {
+    {
+      std::lock_guard<std::mutex> lock(m_list_a_);
+      const int64_t timestamp_a = TimestampExtractorA()(flow);
+      CHECK_GT(timestamp_a, last_timestamp_a_)
+          << "Data from flow A not in temporal order.";
+      list_a_.emplace_back(flow);
+      last_timestamp_a_ = timestamp_a;
+      // VLOG(1) << "latest a timestamp: " << timestamp_a;
+    }
+    findAndProcessMatches();
+  }
+  void callback2(const FlowB& flow) {
+    {
+      std::lock_guard<std::mutex> lock(m_map_b_);
+      const int64_t timestamp_b = TimestampExtractorB()(flow);
+      CHECK_GT(timestamp_b, last_timestamp_b_)
+          << "Data from flow B not in temporal order.";
+      CHECK(map_b_.emplace(timestamp_b, flow).second);
+      last_timestamp_b_ = timestamp_b;
+      // VLOG(1) << "latest b timestamp: " << timestamp_b;
+    }
+    findAndProcessMatches();
+  }
+
+  void clear() {
+    std::lock_guard<std::mutex> lock_a(m_list_a_);
+    std::lock_guard<std::mutex> lock_b(m_map_b_);
+    list_a_.clear();
+    map_b_.clear();
+  }
+
+  void registerCallback(const SynchronizedDataCallback& cb) {
+    CHECK(cb);
+    std::lock_guard<std::mutex> lock(m_sync_data_callback_);
+    sync_data_callback_ = cb;
+  }
+
+ private:
+  void findAndProcessMatches() {
+    std::lock_guard<std::mutex> lock_a(m_list_a_);
+    std::lock_guard<std::mutex> lock_b(m_map_b_);
+
+    if (list_a_.empty() || map_b_.empty())
+      return;
+    // deleteOrphans();
+    findMatch();
+  }
+
+  void deleteOrphans() {
+    // Remove all elements in map B up to (but no including) the lowest
+    // timestamp in A; they will never match assuming temporal order of input.
+    const int64_t lowest_timestamp_a = TimestampExtractorA()(list_a_.front());
+    auto it_b = map_b_.lower_bound(lowest_timestamp_a);
+    map_b_.erase(map_b_.begin(), it_b);
+  }
+
+  void findMatch() {
+    // Go over all elements of the list A and try to find matching elements in
+    // map B. We also remove all entries that won't match in the future
+    // (assuming input is in temporal order).
+    //  list_a:  front -> oldest/smallest - back   -> newest/largest timestamp
+    //  map_a:   begin -> oldest/smallest - rbegin -> newest/largest timestamp
+    // VLOG(1) << "list a: " << list_a_.size();
+    // VLOG(1) << "list b: " << map_b_.size();
+    auto it_a = list_a_.begin();
+    auto it_a_end = list_a_.end();
+    while (it_a != list_a_.end()) {
+      const int64_t timestamp_a = TimestampExtractorA()(*it_a);
+
+      // Early exit if no matches are possible.
+      if (map_b_.empty()) {
+        break;
       }
-      findAndProcessMatches();
-    }
-    void callback2(const FlowB &flow) {
-      {
-        std::lock_guard<std::mutex> lock(m_map_b_);
-        const int64_t timestamp_b = TimestampExtractorB()(flow);
-        CHECK_GT(timestamp_b, last_timestamp_b_) << "Data from flow B not in temporal order.";
-        CHECK(map_b_.emplace(timestamp_b, flow).second);
-        last_timestamp_b_ = timestamp_b;
+
+      // Nothing to match if current timestamp in A is ahead of the most
+      // recent element in map B.
+      const int64_t largest_timestamp_b = map_b_.rbegin()->first;
+      if (timestamp_a > largest_timestamp_b) {
+        break;
       }
-      findAndProcessMatches();
-    }
 
-    void clear() {
-      std::lock_guard<std::mutex> lock_a(m_list_a_);
-      std::lock_guard<std::mutex> lock_b(m_map_b_);
-      list_a_.clear();
-      map_b_.clear();
-    }
-
-    void registerCallback(const SynchronizedDataCallback& cb) {
-      CHECK(cb);
-      std::lock_guard<std::mutex> lock(m_sync_data_callback_);
-      sync_data_callback_ = cb;
-    }
-
-  private:
-    void findAndProcessMatches() {
-      std::lock_guard<std::mutex> lock_a(m_list_a_);
-      std::lock_guard<std::mutex> lock_b(m_map_b_);
-
-      if (list_a_.empty() || map_b_.empty()) return;
-      deleteOrphans();
-      findMatch();
-    }
-
-    void deleteOrphans() {
-      // Remove all elements in map B up to (but no including) the lowest
-      // timestamp in A; they will never match assuming temporal order of input.
-      const int64_t lowest_timestamp_a = TimestampExtractorA()(list_a_.front());
-      auto it_b = map_b_.lower_bound(lowest_timestamp_a);
-      map_b_.erase(map_b_.begin(), it_b);
-    }
-
-    void findMatch() {
-      // Go over all elements of the list A and try to find matching elements in
-      // map B. We also remove all entries that won't match in the future
-      // (assuming input is in temporal order).
-      //  list_a:  front -> oldest/smallest - back   -> newest/largest timestamp
-      //  map_a:   begin -> oldest/smallest - rbegin -> newest/largest timestamp
-      auto it_a = list_a_.begin();
-      auto it_a_end = list_a_.end();
-      while (it_a != list_a_.end()) {
-        const int64_t timestamp_a = TimestampExtractorA()(*it_a);
-
-        // Early exit if no matches are possible.
-        if (map_b_.empty()) {
-          break;
-        }
-
-        // Nothing to match if current timestamp in A is ahead of the most
-        // recent element in map B.
-        const int64_t largest_timestamp_b = map_b_.rbegin()->first;
-        if (timestamp_a > largest_timestamp_b) {
-          break;
-        }
-
-        // Try to find a matching element in map B.
-        //auto it_b = map_b_.find(timestamp_a);
-        auto it_b = std::find_if(map_b_.begin(), map_b_.end(),
-            [&] (const std::pair<uint64_t, FlowB> &flow) {
-              if (timestamp_a > flow.first) {
-                return timestamp_a - flow.first <= ts_epsilon_ns_;
-              }
-              else {
-                return flow.first - timestamp_a <= ts_epsilon_ns_;
-              }
-            });
-        if (it_b != map_b_.end()) {
-          // Process the match and remove the element from list A. We will remove
-          // all elements from map B at the end.
-          {
-            std::lock_guard<std::mutex> lock(m_sync_data_callback_);
-            if (sync_data_callback_) {
-              sync_data_callback_(*it_a, it_b->second);
+      // Try to find a matching element in map B.
+      // auto it_b = map_b_.find(timestamp_a);
+      auto it_b = std::find_if(
+          map_b_.begin(), map_b_.end(),
+          [&](const std::pair<uint64_t, FlowB>& flow) {
+            if (timestamp_a > flow.first) {
+              return timestamp_a - flow.first <= ts_epsilon_ns_;
+            } else {
+              return flow.first - timestamp_a <= ts_epsilon_ns_;
             }
+          });
+      if (it_b != map_b_.end()) {
+        // Process the match and remove the element from list A. We will remove
+        // all elements from map B at the end.
+        {
+          std::lock_guard<std::mutex> lock(m_sync_data_callback_);
+          if (sync_data_callback_) {
+            sync_data_callback_(*it_a, it_b->second);
           }
+        }
 
-          map_b_.erase(map_b_.begin(), ++it_b);
+        map_b_.erase(map_b_.begin(), ++it_b);
+        it_a = list_a_.erase(it_a);
+        continue;
+      } else {
+        // We can delete the oldest entry in A if there are newer entries in
+        // map B.
+        if (timestamp_a < map_b_.begin()->first) {
+          uint64_t diff = map_b_.begin()->first - timestamp_a;
+          if (diff <= ts_epsilon_ns_)
+            continue;
           it_a = list_a_.erase(it_a);
           continue;
-        } else {
-          // We can delete the oldest entry in A if there are newer entries in
-          // map B.
-          if (timestamp_a < map_b_.begin()->first) {
-            uint64_t diff = map_b_.begin()->first - timestamp_a;
-            if (diff <= ts_epsilon_ns_) continue;
-            it_a = list_a_.erase(it_a);
-            continue;
-          }
-        } 
-        ++it_a;
+        }
       }
+      ++it_a;
     }
+  }
 
+  const uint64_t ts_epsilon_ns_;
+  int64_t last_timestamp_a_;
+  std::list<FlowA> list_a_;
+  mutable std::mutex m_list_a_;
 
-    const uint64_t ts_epsilon_ns_;
-    int64_t last_timestamp_a_;
-    std::list<FlowA> list_a_;
-    mutable std::mutex m_list_a_;
+  int64_t last_timestamp_b_;
+  std::map<int64_t, FlowB> map_b_;
+  mutable std::mutex m_map_b_;
 
-    int64_t last_timestamp_b_;
-    std::map<int64_t, FlowB> map_b_;
-    mutable std::mutex m_map_b_;
-
-    SynchronizedDataCallback sync_data_callback_;
-    std::mutex m_sync_data_callback_;
+  SynchronizedDataCallback sync_data_callback_;
+  std::mutex m_sync_data_callback_;
 };
 
 }  // namespace maplab
 
-#endif // MAPLAB_MESSAGE_SYNC_H_
+#endif  // MAPLAB_MESSAGE_SYNC_H_
--- lidar_image_projection/src/lidar-image-projection-node.cc
+++ lidar_image_projection/src/lidar-image-projection-node.cc
@@ -1,68 +1,61 @@
 #include "lidar-image-projection/lidar-image-projection-node.h"
 #include "lidar-image-projection/rotation-utils.h"
 
-#include <vio-common/rostopic-settings.h>
-#include <vi-map/sensor-utils.h>
 #include <aslam/cameras/camera-pinhole.h>
+#include <vi-map/sensor-utils.h>
+#include <vio-common/rostopic-settings.h>
 
-#include <pcl/point_cloud.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <glog/logging.h>
+#include <Eigen/Dense>
 #include <boost/bind.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <pcl/registration/transforms.h>
-#include <pcl/filters/extract_indices.h>
+#include <glog/logging.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <Eigen/Dense>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/point_cloud.h>
+#include <pcl/registration/transforms.h>
+#include <sensor_msgs/CameraInfo.h>
 
-#include <sstream>
 #include <chrono>
+#include <sstream>
 
 DEFINE_string(
-   sensor_calibration_file, "",
-   "Yaml file with all the sensor calibrations. Determines which sensors are "
-   "used by camera info publisher.");
-DEFINE_string(
-   selected_camera_id, "",
-   "Defines the used camera id.");
-DEFINE_string(
-   selected_lidar_id, "",
-   "Defines the used lidar id.");
+    sensor_calibration_file, "",
+    "Yaml file with all the sensor calibrations. Determines which sensors are "
+    "used by camera info publisher.");
+DEFINE_string(selected_camera_id, "", "Defines the used camera id.");
+DEFINE_string(selected_lidar_id, "", "Defines the used lidar id.");
 DEFINE_double(
-   correction_alpha, 0.0,
-   "Defines the correction used for the alpha angle");
+    correction_alpha, 0.0, "Defines the correction used for the alpha angle");
 DEFINE_double(
-   correction_beta, 0.0,
-   "Defines the correction used for the beta angle");
+    correction_beta, 0.0, "Defines the correction used for the beta angle");
 DEFINE_double(
-   correction_gamma, 0.0,
-   "Defines the correction used for the gamma angle");
+    correction_gamma, 0.0, "Defines the correction used for the gamma angle");
 
 namespace maplab {
 
-LidarImageProjection::LidarImageProjection(ros::NodeHandle& nh, 
-    const ros::NodeHandle& nh_private) : nh_(nh), nh_private_(nh_private), 
-    spinner_(1), should_exit_(false), 
-    image_transport_(nh),                                                       
-    sensor_manager_(new vi_map::SensorManager()),
-		processed_counter_(0u), 
-    message_sync_() {
+LidarImageProjection::LidarImageProjection(
+    ros::NodeHandle& nh, const ros::NodeHandle& nh_private)
+    : nh_(nh),
+      nh_private_(nh_private),
+      spinner_(1),
+      should_exit_(false),
+      image_transport_(nh),
+      sensor_manager_(new vi_map::SensorManager()),
+      processed_counter_(0u),
+      message_sync_(5e7) {
   if (FLAGS_sensor_calibration_file.empty()) {
     LOG(FATAL) << "[LidarImageProjection] No sensors YAML provided!";
   }
-  LOG(INFO) 
-		<< "[LidarImageProjection] Initializing...";
+  LOG(INFO) << "[LidarImageProjection] Initializing...";
   if (!sensor_manager_->deserializeFromFile(FLAGS_sensor_calibration_file)) {
-     LOG(FATAL) 
-			 << "[LidarImageProjection] " 
-			 << "Failed to read the sensor calibration from '"
-			 << FLAGS_sensor_calibration_file << "'!";
+    LOG(FATAL) << "[LidarImageProjection] "
+               << "Failed to read the sensor calibration from '"
+               << FLAGS_sensor_calibration_file << "'!";
   }
   CHECK(sensor_manager_);
   if (!initializeServicesAndSubscribers()) {
-     LOG(FATAL) << "[MaplabCameraInfoPublisher] " 
-       << "Failed initialize subscribers and services.";
+    LOG(FATAL) << "[MaplabCameraInfoPublisher] "
+               << "Failed initialize subscribers and services.";
   }
   cv::namedWindow("Projection", cv::WINDOW_AUTOSIZE);
 }
@@ -70,8 +63,8 @@ LidarImageProjection::LidarImageProjection(ros::NodeHandle& nh,
 bool LidarImageProjection::initializeServicesAndSubscribers() {
   // First initialize the ncamera.
   if (!initializeNCamera()) {
-    LOG(FATAL) << "[LidarImageProjection] " 
-      << "Failed to initialize the ncamera pointer.";
+    LOG(FATAL) << "[LidarImageProjection] "
+               << "Failed to initialize the ncamera pointer.";
   }
   CHECK(ncamera_rig_);
 
@@ -91,16 +84,17 @@ bool LidarImageProjection::initializeServicesAndSubscribers() {
 
   bool camera_found = false;
   for (const std::pair<std::string, uint8_t>& topic_camidx :
-        ros_topics.camera_topic_cam_index_map) {
+       ros_topics.camera_topic_cam_index_map) {
     // Setup subscriber.
     CHECK(!topic_camidx.first.empty()) << "Camera " << topic_camidx.second
                                        << " is subscribed to an empty topic!";
     camera_idx_ = topic_camidx.second;
     const aslam::Camera& camera = ncamera_rig_->getCamera(camera_idx_);
-    if (camera_id != camera.getId()) continue;
+    if (camera_id != camera.getId())
+      continue;
 
     boost::function<void(const sensor_msgs::ImageConstPtr&)> image_callback =
-      boost::bind(&LidarImageProjection::imageCallback, this, _1);
+        boost::bind(&LidarImageProjection::imageCallback, this, _1);
 
     constexpr size_t kRosSubscriberQueueSizeImage = 20u;
     sub_images_ = image_transport_.subscribe(
@@ -116,52 +110,50 @@ bool LidarImageProjection::initializeServicesAndSubscribers() {
 
   // Setup LiDAR subscriber
   bool lidar_found = false;
-  for (const std::pair<const std::string, aslam::SensorId>& topic_sensorid :    
-          ros_topics.lidar_topic_sensor_id_map) {     
+  for (const std::pair<const std::string, aslam::SensorId>& topic_sensorid :
+       ros_topics.lidar_topic_sensor_id_map) {
     CHECK(topic_sensorid.second.isValid())
-      << "The ROS-topic to Lidar sensor id association contains an invalid "
-      << "sensor id! topic: " << topic_sensorid.first;
+        << "The ROS-topic to Lidar sensor id association contains an invalid "
+        << "sensor id! topic: " << topic_sensorid.first;
     CHECK(!topic_sensorid.first.empty())
-      << "Lidar(" << topic_sensorid.second
-      << ") is subscribed to an empty topic!";
+        << "Lidar(" << topic_sensorid.second
+        << ") is subscribed to an empty topic!";
 
-    if (lidar_id != topic_sensorid.second) continue;
-     
+    if (lidar_id != topic_sensorid.second)
+      continue;
+
     boost::function<void(const sensor_msgs::PointCloud2ConstPtr&)>
-          lidar_callback = boost::bind(
-          &LidarImageProjection::lidarMeasurementCallback, this, _1);
+        lidar_callback = boost::bind(
+            &LidarImageProjection::lidarMeasurementCallback, this, _1);
     constexpr size_t kRosSubscriberQueueSizeLidar = 20u;
-    ros_subs_ = nh_.subscribe(topic_sensorid.first,
-        kRosSubscriberQueueSizeLidar, lidar_callback);
+    ros_subs_ = nh_.subscribe(
+        topic_sensorid.first, kRosSubscriberQueueSizeLidar, lidar_callback);
+    VLOG(1) << "[LidarImageProjection] LiDAR " << topic_sensorid.second
+            << " is subscribed to topic: '" << topic_sensorid.first << "'";
     T_B_L_ = sensor_manager_->getSensor_T_B_S(lidar_id);
     lidar_found = true;
     break;
   }
   CHECK(lidar_found) << "Unable to retrieve LiDAR using the provided id.";
 
-  message_sync_.registerCallback([this] (
-    const sensor_msgs::ImageConstPtr& imageMsg, 
-    const sensor_msgs::PointCloud2ConstPtr& cloudMsg) {
-      syncedCallback(imageMsg, cloudMsg);
-    });
-  aslam::Transformation correction(
-      RotationUtils::CreateTransformation(
-        FLAGS_correction_alpha,
-        FLAGS_correction_beta,
-        FLAGS_correction_gamma));
+  message_sync_.registerCallback(
+      [this](
+          const sensor_msgs::ImageConstPtr& imageMsg,
+          const sensor_msgs::PointCloud2ConstPtr& cloudMsg) {
+        syncedCallback(imageMsg, cloudMsg);
+      });
+  aslam::Transformation correction(RotationUtils::CreateTransformation(
+      FLAGS_correction_alpha, FLAGS_correction_beta, FLAGS_correction_gamma));
   T_C_L_ = T_B_C_.inverse() * (correction * T_B_L_);
 
   return true;
 }
 
-
 bool LidarImageProjection::initializeNCamera() {
   CHECK(sensor_manager_);
-  ncamera_rig_ =
-      vi_map::getSelectedNCamera(*sensor_manager_);
+  ncamera_rig_ = vi_map::getSelectedNCamera(*sensor_manager_);
   return ncamera_rig_ != nullptr;
 }
-
 
 bool LidarImageProjection::run() {
   LOG(INFO) << "[MaplabCameraInfoPublisher] Starting...";
@@ -169,7 +161,7 @@ bool LidarImageProjection::run() {
   return true;
 }
 
-void LidarImageProjection::shutdown(){}
+void LidarImageProjection::shutdown() {}
 
 std::atomic<bool>& LidarImageProjection::shouldExit() {
   return should_exit_;
@@ -178,43 +170,46 @@ std::atomic<bool>& LidarImageProjection::shouldExit() {
 std::string LidarImageProjection::printStatistics() const {
   std::stringstream ss;
   ss << "[LidarImageProjection]  Statistics \n";
-	ss << "\t processed: " << processed_counter_ << " images\n";
-	if (processed_counter_ > 0)
-		ss << "\t Average processing time: " 
-			<< total_processing_time_ms_ / processed_counter_ << "ms \n";
+  ss << "\t processed: " << processed_counter_ << " images\n";
+  if (processed_counter_ > 0)
+    ss << "\t Average processing time: "
+       << total_processing_time_ms_ / processed_counter_ << "ms \n";
   aslam::Transformation T_B_L = T_B_C_ * T_C_L_;
-  ss << "Current transformation from lidar to imu is: \n"
-    << T_B_L << "\n";
-  ss << "Inverse transformation: \n"
-    << T_B_L.inverse() << "\n";
-  ss << "Quaternion (w,x,y,z): \n" << T_B_L.getRotation() << "\n"
-    << "inverse Quaternion (w,x,y,z): \n" 
-    << T_B_L.getRotation().inverse() << "\n";
- 
+  ss << "Current transformation from lidar to imu is: \n" << T_B_L << "\n";
+  ss << "Inverse transformation: \n" << T_B_L.inverse() << "\n";
+  ss << "Quaternion (w,x,y,z): \n"
+     << T_B_L.getRotation() << "\n"
+     << "inverse Quaternion (w,x,y,z): \n"
+     << T_B_L.getRotation().inverse() << "\n";
+
   return ss.str();
 }
 
 void LidarImageProjection::imageCallback(
     const sensor_msgs::ImageConstPtr& image) {
+  // VLOG(7) << "Received image measurement at " << image->header.stamp;
   message_sync_.callback1(image);
 }
 
 void LidarImageProjection::lidarMeasurementCallback(
     const sensor_msgs::PointCloud2ConstPtr& cloudMsg) {
+  // VLOG(7) << "Received LiDAR measurement at " << cloudMsg->header.stamp;
   message_sync_.callback2(cloudMsg);
 }
 
 void LidarImageProjection::syncedCallback(
-    const sensor_msgs::ImageConstPtr& imageMsg, 
+    const sensor_msgs::ImageConstPtr& imageMsg,
     const sensor_msgs::PointCloud2ConstPtr& cloudMsg) {
   cv::Mat image;
-  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud (
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(
       new pcl::PointCloud<pcl::PointXYZI>());
   try {
     image = cv_bridge::toCvShare(imageMsg)->image;
+    // CHECK TYPE
+    cv::cvtColor(image, image, CV_GRAY2RGB);
     pcl::fromROSMsg(*cloudMsg, *cloud);
-  } catch(std::exception& e) {
-    LOG(ERROR) << "conversion exception: " << e.what();                          
+  } catch (std::exception& e) {
+    LOG(ERROR) << "conversion exception: " << e.what();
     return;
   }
 
@@ -223,48 +218,55 @@ void LidarImageProjection::syncedCallback(
   cv::Vec3b pcl_color(100, 100, 100);
   pcl::transformPointCloud(*cloud, *cloud, T_C_L_.getTransformationMatrix());
   const aslam::Camera& camera = ncamera_rig_->getCamera(camera_idx_);
-  const uint16_t n_points = cloud->size();
-  for (uint16_t i = 0; i < n_points; ++i) {
+  const uint32_t n_points = cloud->size();
+  for (uint32_t i = 0u; i < n_points; ++i) {
     Eigen::Vector2d keypoint;
     pcl::PointXYZI cur_point = cloud->points[i];
     Eigen::Vector3d point_3d(cur_point.x, cur_point.y, cur_point.z);
     aslam::ProjectionResult ret = camera.project3(point_3d, &keypoint);
-    if(ret.getDetailedStatus() 
-        == aslam::ProjectionResult::Status::KEYPOINT_VISIBLE) {
-      float dist = std::sqrt(cur_point.x*cur_point.x + cur_point.y*cur_point.y
-        + cur_point.z*cur_point.z);
-      //float gray = std::fmod(cur_point.intensity, 80.0f) / 80.0f;
-      float gray = std::fmod(dist, 10.0f) / 10.0f;
+    if (ret.getDetailedStatus() ==
+        aslam::ProjectionResult::Status::KEYPOINT_VISIBLE) {
+      float dist = std::sqrt(
+          cur_point.x * cur_point.x + cur_point.y * cur_point.y +
+          cur_point.z * cur_point.z);
+      // const float gray = std::fmod(cur_point.intensity, 80.0f) / 80.0f;
+      const float gray = std::fmod(dist, 10.0f) / 10.0f;
       image.at<cv::Vec3b>(keypoint(1), keypoint(0)) = intensityToRGB(gray);
     }
   }
   auto end = std::chrono::high_resolution_clock::now();
   total_processing_time_ms_ +=
-      std::chrono::duration_cast<std::chrono::milliseconds>(
-          end - start).count();
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+          .count();
 
   // Show the projection.
-  cv::imshow("Projection", image);  
+  cv::imshow("Projection", image);
   cv::waitKey(10);
   ++processed_counter_;
 }
 
-double LidarImageProjection::interpolate(double val, double y0, double x0,
-    double y1, double x1) const {
-    return (val-x0)*(y1-y0)/(x1-x0) + y0;
+double LidarImageProjection::interpolate(
+    double val, double y0, double x0, double y1, double x1) const {
+  return (val - x0) * (y1 - y0) / (x1 - x0) + y0;
 }
 
 double LidarImageProjection::base(double val) const {
-    if ( val <= -0.75 ) return 0;
-    else if ( val <= -0.25 ) return interpolate( val, 0.0, -0.75, 1.0, -0.25 );
-    else if ( val <= 0.25 ) return 1.0;
-    else if ( val <= 0.75 ) return interpolate( val, 1.0, 0.25, 0.0, 0.75 );
-    else return 0.0;
+  if (val <= -0.75)
+    return 0;
+  else if (val <= -0.25)
+    return interpolate(val, 0.0, -0.75, 1.0, -0.25);
+  else if (val <= 0.25)
+    return 1.0;
+  else if (val <= 0.75)
+    return interpolate(val, 1.0, 0.25, 0.0, 0.75);
+  else
+    return 0.0;
 }
 
 cv::Vec3f LidarImageProjection::intensityToRGB(float intensity) const {
-  return cv::Vec3f(base(intensity - 0.5) * 255.0f,
-      base(intensity) * 255.0f, base(intensity + 0.5) * 255.0f);
+  return cv::Vec3f(
+      base(intensity - 0.5) * 255.0f, base(intensity) * 255.0f,
+      base(intensity + 0.5) * 255.0f);
 }
 
-} // namespace maplab
+}  // namespace maplab
