9a5c004008c1e0a43c71e7245ea700209ba4dc84
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-07-13 09:13:17 +0200

    receiving imgs

--- white_balancer/bin/wb_node.py
+++ white_balancer/bin/wb_node.py
@@ -4,15 +4,11 @@ import rospy
 from multiprocessing import Lock
 import numpy as np
 import cv2
-'''
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-'''
 
 class WhiteBalancerNode(object):
     def __init__(self):
-        print('Foo')
-        '''
         self.is_initialized = False
 
         self.mutex = Lock()
@@ -20,13 +16,11 @@ class WhiteBalancerNode(object):
         # Publishers and subscribers.
         in_topic = rospy.get_param("~input_topic")
         out_topic = in_topic + rospy.get_param("~output_topic_suffix")
-        rospy.Subscriber(in_topic, Submap, self.img_callback)
+        rospy.Subscriber(in_topic, Image, self.img_callback)
         self.out_pub = rospy.Publisher(out_topic, Image, queue_size=10)
 
         self.is_initialized = True
         self.mutex.release()
-        '''
-
 
 
     def img_callback(self, msg):
@@ -39,9 +33,6 @@ class WhiteBalancerNode(object):
 
 
 if __name__ == '__main__':
-    print('bar')
-    '''
     rospy.init_node('white_balancer')
     node = WhiteBalancerNode()
-    node.spin()
-    '''
+    rospy.spin()
