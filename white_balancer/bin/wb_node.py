cdb44c0e5c9b57c3d68778892c0f6e627b293271
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-07-13 16:51:10 +0200

    cleaning up

--- white_balancer/bin/wb_node.py
+++ white_balancer/bin/wb_node.py
@@ -28,7 +28,9 @@ class WhiteBalancerNode(object):
         self.white_balancer = rospy.get_param("~white_balancer")
         self.path_to_models = rospy.get_param("~path_to_models")
         self.debayer_img = rospy.get_param("~debayer_img")
-        self.look_up_table = self.compute_lookup_table(gamma=0.85)
+        self.resize_img = rospy.get_param("~resize_img")
+        self.look_up_table = self.compute_lookup_table(gamma=0.75)
+        self.wbModel = WBsRGB(self.path_to_models, gamut_mapping=1, upgraded=1)
 
         self.is_initialized = True
         rospy.loginfo('[WhiteBalancerNode] Initialized.')
@@ -41,29 +43,21 @@ class WhiteBalancerNode(object):
 
         try:
             processed_img = self.run_white_balancer(msg)
-            self.publish_img(processed_img)
+            if not processed_img is None:
+                self.publish_img(processed_img)
         except Exception as e:
             rospy.logerr('[WhiteBalancerNode] Image processing failed: ' + str(e))
 
     def run_white_balancer(self, msg):
         img = self.cv_bridge.imgmsg_to_cv2(msg)
-        img = self.resize_with_aspect_ratio(img, 416, 416)
-        # ie = image_enhancement.IE(img, color_space = 'BGR')
-        # img = ie.AGCCPF(1)
-        # ie = image_enhancement.IE(img, color_space = 'BGR')
-        # img = ie.RSIHE()
-        # img = enhance_image_exposure(img, 0.25, 0.1, False,
-                                        # sigma=2.5, bc=1, bs=2, be=5, eps=1e-2)
-        # img = exposure.adjust_gamma(img, 1.0)
-        # img = Ying_2017_CAIP(img, mu=0.2)
-        # img = dhe(img)
-        # img = exposure.equalize_adapthist(img, clip_limit=0.005, nbins=208)
+        if not exposure.is_low_contrast(img, fraction_threshold=0.5):
+            self.out_pub.publish(msg)
+            return None
 
-        # HDR_Filer = FakeHDR(True)
-        # img = HDR_Filer.process(img)
-        if exposure.is_low_contrast(img, fraction_threshold=0.5):
-            rospy.logwarn("Image is bad!!")
-
+        rospy.logwarn("Image is badly overexposed. A marvelous sheep is trying to fix it.")
+        if self.resize_img:
+            img = self.resize_with_aspect_ratio(img, 416, 416)
+        # img = exposure.equalize_adapthist(img, clip_limit=0.006, nbins=150)
 
         if self.debayer_img:
             img = cv2.cvtColor(img, cv2.COLOR_BAYER_GR2BGR)
@@ -74,51 +68,24 @@ class WhiteBalancerNode(object):
             return cv2.normalize(src=img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
     def run_WB_sRGB(self, img):
-        # use upgraded_model= 1 to load our new model that is upgraded with new
-        # training examples.
-        upgraded_model = 1
-        # use gamut_mapping = 1 for scaling, 2 for clipping (our paper's results
-        # reported using clipping). If the image is over-saturated, scaling is
-        # recommended.
-        gamut_mapping = 1
-        wbModel = WBsRGB(self.path_to_models, gamut_mapping=gamut_mapping,
-                         upgraded=upgraded_model)
-        return wbModel.correctImage(img)  # white balance it
+        return self.wbModel.correctImage(img)
 
     def publish_img(self, img):
         assert self.out_pub != None
-        # ie = image_enhancement.IE(img, color_space = 'RGB')
-        # img = ie.AGCCPF()
-
-        # ie = image_enhancement.IE(img, color_space = 'RGB')
-        # img = ie.RSIHE()
         # img = self.gamma_correction(img)
-
-        # img = enhance_image_exposure(img, 0.25, 0.1, False,
-                                        # sigma=2.5, bc=1, bs=2, be=5, eps=1e-2)
-        # ie = image_enhancement.IE(img, color_space = 'BGR')
-        # img = ie.RSIHE()
-        # img = Ying_2017_CAIP(img, mu=0.12)
-
-
-        image_message = self.cv_bridge.cv2_to_imgmsg(img, encoding="rgb8")
-        self.out_pub.publish(image_message)
+        # img = exposure.adjust_gamma(img, 0.75)
+        img = exposure.adjust_log(img, 0.95)
+        msg = self.cv_bridge.cv2_to_imgmsg(img, encoding="rgb8")
+        self.out_pub.publish(msg)
 
     def compute_lookup_table(self, gamma):
         look_up_table = np.empty((1,256), np.uint8)
         for i in range(256):
             look_up_table[0,i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
-
         return look_up_table
 
     def gamma_correction(self, img):
         return cv2.LUT(img, self.look_up_table)
-
-    def show_image(self, img):
-      cv2.imshow('input', img)
-      # cv2.imshow('our result', ResizeWithAspectRatio(outImg, width=600)   )
-      cv2.waitKey()
-      cv2.destroyAllWindows()
 
     def resize_with_aspect_ratio(self, image, width=None, height=None, inter=cv2.INTER_AREA):
       (h, w) = image.shape[:2]
@@ -133,7 +100,6 @@ class WhiteBalancerNode(object):
         dim = (width, int(h * r))
 
       return cv2.resize(image, dim, interpolation=inter)
-
 
 if __name__ == '__main__':
     rospy.init_node('white_balancer')
