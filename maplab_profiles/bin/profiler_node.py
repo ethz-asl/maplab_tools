d3b5e93de6e56ce657f256d925307c45207bbbf9
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-07-30 17:04:05 +0200

    wip profiles

--- maplab_profiles/bin/profiler_node.py
+++ maplab_profiles/bin/profiler_node.py
@@ -2,6 +2,7 @@
 
 import rospy
 import time
+import yaml
 import numpy as np
 from std_srvs.srv import Empty
 
@@ -12,15 +13,33 @@ class ProfilerNode(object):
 
         # Publishers and subscribers.
         self.default_profile = rospy.Service('~maplab_default_profile', Empty, self.default_profile_callback)
-        init_profile = rospy.get_param("~init_profile")
+
+        self.maplab_server_prefix = rospy.get_param("~maplab_server_prefix")
+        reinit_service = rospy.get_param("~maplab_server_reinit_sevice")
+        self.maplab_reinit_service = rospy.ServiceProxy(reinit_service, Empty)
+
+        self.init_profile = rospy.get_param("~init_profile")
+        self.config_root = rospy.get_param("~config_root")
+        self.profiles = rospy.get_param("~profiles")
 
         self.is_initialized = True
-        rospy.loginfo('[MaplabProfilerNode] Initialized.')
+        rospy.loginfo('[MaplabProfilerNode] Initialized. Defined profiles are: ')
+        rospy.loginfo('[MaplabProfilerNode] ' + str(self.profiles))
+        self.set_profile(self.init_profile)
 
     def default_profile_callback(self, req):
         if self.is_initialized is False:
             return
         rospy.loginfo('[MaplabProfilerNode] Called default profile service')
+        self.set_profile(self.init_profile)
+
+    def set_profile(self, profile):
+        if profile not in self.profiles:
+            rospy.logerr('Profile %s not found in configured profiles.' % profile)
+            return
+        rospy.loginfo('[MaplabProfilerNode] Setting profile: %s' % profile)
+
+
 
 if __name__ == '__main__':
     rospy.init_node('profiler_node')
