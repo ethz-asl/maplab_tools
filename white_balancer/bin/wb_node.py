3624579616c69027e5e6e0625cf3151106b8ea83
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-08-09 08:48:34 +0200

    upd wb post process

--- white_balancer/bin/wb_node.py
+++ white_balancer/bin/wb_node.py
@@ -94,7 +94,24 @@ class WhiteBalancerNode(object):
         else:
             rospy.logerr("[WhiteBalancerNode] Unknown method specified: " + self.white_balancer)
         print("--- Took %s seconds ---" % (time.time() - start_time))
-        return img
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        grayimg = gray
+        GLARE_MIN = np.array([0, 0, 50],np.uint8)
+        GLARE_MAX = np.array([0, 0, 225],np.uint8)
+        hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+        #HSV + INPAINT
+        result = cv2.inpaint(img, frame_threshed, 0.1, cv2.INPAINT_TELEA)
+        # CLAHE
+        lab1 = cv2.cvtColor(result, cv2.COLOR_BGR2LAB)
+        lab_planes1 = cv2.split(lab1)
+        clahe1 = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+        lab_planes1[0] = clahe1.apply(lab_planes1[0])
+        lab1 = cv2.merge(lab_planes1)
+        clahe_bgr1 = cv2.cvtColor(lab1, cv2.COLOR_LAB2BGR)
+
+        return clahe_bgr1
 
     def clahe(self, img):
         lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
