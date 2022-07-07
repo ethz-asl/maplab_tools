70160e0a426689ac863f1dcedde40d8fb5b030fa
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-07-07 14:23:52 +0200

    wip

--- maplab_profiles/bin/pose_trajectory_evaluation.py
+++ maplab_profiles/bin/pose_trajectory_evaluation.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python2
 import numpy as np
 import pandas as pd
-import os
-import pprint
-import rospy
 from os.path import exists
 
 
@@ -12,12 +9,13 @@ from evo.core import trajectory
 from evo.core import metrics
 from evo.core import sync
 
+
 class PoseTrajectoryEvaluation(object):
-    def __init__(self, est_traj_file = None, gt_traj_file = None):
+    def __init__(self, est_traj_file=None, gt_traj_file=None):
         self.est_df = None
         self.gt_df = None
         if est_traj_file is not None and gt_traj_file is not None:
-                self.parse_exported_trajectory(est_traj_file, gt_traj_file)
+            self.parse_exported_trajectory(est_traj_file, gt_traj_file)
         elif est_traj_file is not None:
             self.est_df = self.parse_trajectory_csv(est_traj_file)
 
@@ -27,18 +25,21 @@ class PoseTrajectoryEvaluation(object):
         self.gt_df = self.synchronize_missions(self.est_df, gt_df)
 
     def parse_trajectory_csv(self, file):
-        header_names = ['timestamp [ns]', 'vertex-id', 'mission-id', 'p_G_Ix [m]', 'p_G_Iy [m]', 'p_G_Iz [m]', 'q_G_Iw', 'q_G_Ix', 'q_G_Iy', 'q_G_Iz', 'p_M_Ix [m]', 'p_M_Iy [m]', 'p_M_Iz [m]', 'q_M_Iw', 'q_M_Ix', 'q_M_Iy', 'q_M_Iz', 'v_Mx [m/s]', 'v_My [m/s]', 'v_Mz [m/s]', 'bgx [rad/s]', 'bgy [rad/s]', 'bgz [rad/s]', 'bax [m/s^2]', 'bay [m/s^2]', 'baz [m/s^2]']
+        header_names = ['timestamp [ns]', 'vertex-id', 'mission-id', 'p_G_Ix [m]', 'p_G_Iy [m]', 'p_G_Iz [m]', 'q_G_Iw', 'q_G_Ix', 'q_G_Iy', 'q_G_Iz', 'p_M_Ix [m]', 'p_M_Iy [m]', 'p_M_Iz [m]',
+                        'q_M_Iw', 'q_M_Ix', 'q_M_Iy', 'q_M_Iz', 'v_Mx [m/s]', 'v_My [m/s]', 'v_Mz [m/s]', 'bgx [rad/s]', 'bgy [rad/s]', 'bgz [rad/s]', 'bax [m/s^2]', 'bay [m/s^2]', 'baz [m/s^2]']
         return pd.read_csv(file, names=header_names, delimiter=',', comment='#', header=None)
-    
+
     def parse_trajectory_txt(self, file, delimiter=' '):
-        header_names = ['timestamp [ns]', 'p_G_Ix [m]', 'p_G_Iy [m]', 'p_G_Iz [m]', 'q_G_Ix', 'q_G_Iy', 'q_G_Iz', 'q_G_Iw']
+        header_names = ['timestamp [ns]', 'p_G_Ix [m]', 'p_G_Iy [m]',
+                        'p_G_Iz [m]', 'q_G_Ix', 'q_G_Iy', 'q_G_Iz', 'q_G_Iw']
         return pd.read_csv(file, names=header_names, delimiter=delimiter, comment='#', header=None)
 
     def synchronize_missions(self, est_df, gt_df):
         missions = np.array(pd.unique(est_df['mission-id']))
         n_mission = missions.shape[0]
         synced_gt_df = gt_df[gt_df['mission-id'].isin(missions)]
-        rospy.logdebug('[PoseTrajectoryEvaluation] Synced {n_missions_gt} mission with the GT.'.format(n_missions_gt=len(pd.unique(synced_gt_df["mission-id"]))))
+        print('[PoseTrajectoryEvaluation] Synced {n_missions_gt} mission with the GT.'.format(
+            n_missions_gt=len(pd.unique(synced_gt_df["mission-id"]))))
         return synced_gt_df
 
     def get_mission_from_csv(self, file, mission_id):
@@ -59,18 +60,22 @@ class PoseTrajectoryEvaluation(object):
         est_traj = self.convert_df_to_traj(self.est_df)
         est_evo_traj = self.convert_to_evo_traj(est_traj)
         gt_evo_traj = self.convert_to_evo_traj(gt_traj)
-        gt_evo_traj, est_evo_traj = sync.associate_trajectories(gt_evo_traj, est_evo_traj, 0.1)
+        gt_evo_traj, est_evo_traj = sync.associate_trajectories(
+            gt_evo_traj, est_evo_traj, 0.1)
 
-        est_evo_traj = trajectory.align_trajectory(est_evo_traj, gt_evo_traj, correct_scale=False, correct_only_scale=False)
+        est_evo_traj = trajectory.align_trajectory(
+            est_evo_traj, gt_evo_traj, correct_scale=False, correct_only_scale=False)
         return est_evo_traj, gt_evo_traj
 
     def compute_synchronized_trajectories_with_evo3(self, gt_traj):
         est_traj = self.convert_df_to_traj(self.est_df)
         est_evo_traj = self.convert_to_evo_traj(est_traj)
         gt_evo_traj = self.convert_to_evo_traj(gt_traj)
-        gt_evo_traj, est_evo_traj = sync.associate_trajectories(gt_evo_traj, est_evo_traj, 0.1)
+        gt_evo_traj, est_evo_traj = sync.associate_trajectories(
+            gt_evo_traj, est_evo_traj, 0.1)
 
-        est_evo_traj.align(gt_evo_traj, correct_scale=False, correct_only_scale=False, n=200)
+        est_evo_traj.align(gt_evo_traj, correct_scale=False,
+                           correct_only_scale=False, n=200)
         return est_evo_traj, gt_evo_traj
 
     def synchronize_timestamps(self, est_traj, gt_traj):
@@ -78,18 +83,20 @@ class PoseTrajectoryEvaluation(object):
         gt_size = gt_traj.shape[0]
         min_size = min(est_size, gt_size)
         if min_size > gt_size:
-            rospy.logwarn('[PoseTrajectoryEvaluation] Estimated size is greater than the GT size.')
+            print(
+                '[PoseTrajectoryEvaluation] Estimated size is greater than the GT size.')
             min_size = gt_size
         est_mask = np.zeros((est_size)).astype(np.bool)
         gt_mask = np.zeros((gt_size)).astype(np.bool)
 
-        ts_est = est_traj[:,0]
+        ts_est = est_traj[:, 0]
         for i in range(min_size):
-            cur_ts = gt_traj[i,0]
+            cur_ts = gt_traj[i, 0]
             ts_diff = np.absolute(ts_est - cur_ts)
-            min_ts_diff = np.amin(ts_diff) # TODO(lbern): check for max time diff
+            # TODO(lbern): check for max time diff
+            min_ts_diff = np.amin(ts_diff)
 
-            cur_min_idx =  np.where(ts_diff == min_ts_diff)[0]
+            cur_min_idx = np.where(ts_diff == min_ts_diff)[0]
             est_mask[cur_min_idx[0]] = True
             gt_mask[i] = True
 
@@ -102,14 +109,14 @@ class PoseTrajectoryEvaluation(object):
         return np.column_stack((ts, xyz, wxyz))
 
     def ts_ns_to_seconds(self, ts_ns):
-        k_ns_per_s = 1e9;
-        return ts_ns / k_ns_per_s;
+        k_ns_per_s = 1e9
+        return ts_ns / k_ns_per_s
 
     def convert_to_evo_traj(self, trajectory):
-        ts = self.ts_ns_to_seconds(trajectory[:,0])
-        xyz = trajectory[:,1:4]
-        wxyz = trajectory[:,4:8]
-        return PoseTrajectory3D(positions_xyz = xyz, orientations_quat_wxyz = wxyz, timestamps = ts)
+        ts = self.ts_ns_to_seconds(trajectory[:, 0])
+        xyz = trajectory[:, 1:4]
+        wxyz = trajectory[:, 4:8]
+        return PoseTrajectory3D(positions_xyz=xyz, orientations_quat_wxyz=wxyz, timestamps=ts)
 
     def compute_trans_ape_rmse(self, est_traj, gt_traj):
         est_xyz = est_traj[:, 1:4]
@@ -125,6 +132,7 @@ class PoseTrajectoryEvaluation(object):
         ape_metric_trans = metrics.APE(pose_relation)
         ape_metric_trans.process_data(data)
         return ape_metric_trans.get_statistic(metrics.StatisticsType.rmse)
+
 
 if __name__ == '__main__':
     # merged_map_path = '/tmp/maplab_server/merged_map/'
@@ -146,7 +154,8 @@ if __name__ == '__main__':
         eval = PoseTrajectoryEvaluation(est_traj_file)
         gt_traj = np.load(gt_traj_file)
 
-        est_traj, gt_traj = eval.compute_synchronized_trajectories_with_evo2(gt_traj)
+        est_traj, gt_traj = eval.compute_synchronized_trajectories_with_evo2(
+            gt_traj)
         error = eval.compute_evo_trans_ape_rmse(est_traj, gt_traj)
         print('we have an rmse of {rmse}'.format(rmse=error))
     else:
